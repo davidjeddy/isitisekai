57bc5fc1244e3f3e7786ad238ab9951303ae1afe
David J Eddy <me@davidjeddy.com>
2022-08-11 11:41:07 +0200

    init swap out of cloud posse module with aws provided source.

---
+++ terraform/prd/variables.tf
@@ -0,0 +1,37 @@
+## Generic
+
+variable "aws_acct_id" {
+  default     = 138402329854
+  description = "AWS account ID"
+  type        = number
+}
+
+variable "delimiter" {
+  default     = "_"
+  description = "Delimiter character"
+  type        = string
+}
+
+variable "name" {
+  default     = "isitisekai"
+  description = "Project name"
+  type        = string
+}
+
+variable "region" {
+  default     = "us-east-1"
+  description = "Home AWS region"
+  type        = string
+}
+
+variable "stage" {
+  default     = "prd"
+  description = "The stage; aka environment"
+  type        = string
+}
+
+variable "tags" {
+  default     = {}
+  description = "Default shared tags"
+  type        = map(any)
+}
