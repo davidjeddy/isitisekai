6ea480dbb6b94ea15da5e84ed1442f9377521724
David J Eddy <me@davidjeddy.com>
2022-08-11 12:35:34 +0200

    Updated providers.

--- terraform/prd/main.tf
+++ terraform/prd/main.tf
@@ -1,20 +1,3 @@
-provider "aws" {
-  profile                 = var.name
-  region                  = var.region
-  shared_credentials_file = "$HOME/.aws/credentials"
-
-  # Make it faster by skipping something
-  # skip_credentials_validation = true
-  # skip_get_ec2_platforms      = true
-  # skip_metadata_api_check     = true
-  # skip_region_validation      = true
-  # skip_requesting_account_id  = false # skip_requesting_account_id should be disabled to generate valid ARN in apigatewayv2_api_execution_arn
-
-  default_tags {
-    tags = var.tags
-  }
-}
-
 locals {
   domain_name = join(".", [var.name, "com"])
   subdomain   = "www"
--- terraform/prd/terraform.tf
+++ terraform/prd/terraform.tf
@@ -7,20 +7,20 @@ terraform {
     }
   }
 
-  required_version = ">= 0.13.1"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.64"
+      version = "~> 3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.0"
+      version = "~> 3.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 2.0"
+      version = "~> 3.0"
     }
   }
+
+  required_version = "~> 1.2.0"
 }
